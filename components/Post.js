c6af054a80ba6d22d900ef3e326940cb3a5aadbf
Murial Anindo <murial.anindo@lonett.com>
2022-10-10 08:37:24 -0400

    new changes for deployment

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Post({ post }) {
   return (
     <div className="card">
-      <img src={post.frontmatter.cover_image} alt="" />
+      <Image
+        src={post.frontmatter.cover_image}
+        alt=""
+        width={400}
+        height={500}
+      />
 
       <div className="post-date">Posted on {post.frontmatter.date}</div>
 
