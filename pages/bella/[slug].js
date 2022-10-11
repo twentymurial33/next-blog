4ebe24741a0c816cc4a47060b9ae9eb6ad815a3c
Murial Anindo <murial.anindo@lonett.com>
2022-10-11 14:19:43 -0400

    final hooray

--- pages/bella/[slug].js
+++ pages/bella/[slug].js
@@ -1,17 +1,40 @@
 import fs from "fs";
 import path from "path";
+import matter from "gray-matter";
+import marked from "marked";
+import Link from "next/link";
 
-function postPage() {
-  return <div>post</div>;
+export default function PostPage({
+  frontmatter: { title, date, cover_image },
+  slug,
+  content,
+}) {
+  return (
+    <>
+      <Link href="/">
+        <a className="btn btn-back">Go Back</a>
+      </Link>
+      <div className="card card-page">
+        <h1 className="post-title">{title}</h1>
+        <div className="post-date">Posted on {date}</div>
+        <img src={cover_image} alt="" />
+        <div className="post-body">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export async function getStaticPaths() {
   const files = fs.readdirSync(path.join("posts"));
+
   const paths = files.map((filename) => ({
     params: {
       slug: filename.replace(".md", ""),
     },
   }));
+
   return {
     paths,
     fallback: false,
@@ -19,7 +42,18 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  return slug;
-}
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
 
-export default postPage;
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      frontmatter,
+      slug,
+      content,
+    },
+  };
+}
